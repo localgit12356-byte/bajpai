3a4355fe5b2575455847e0395cc2db7b99edba8b
unknown <aman@gmail.com>
2026-03-14 11:20:18 +0530

    fuction

---
+++ data.java
@@ -0,0 +1,14 @@
+public class data {
+
+    static void sum(){
+        int a=45;
+        int b=66;
+        int c=a+b;
+        System.out.println(c);
+
+
+    }
+    public static void main(String args[]){
+        sum();
+    }
+}
