eacb23ee85d254d37fc92b1ec5dfce76e14f4c3c
unknown <aman@gmail.com>
2026-03-14 12:02:16 +0530

    switch

--- data.java
+++ data.java
@@ -10,5 +10,30 @@ public class data {
     }
     public static void main(String args[]){
         sum();
+        int x=4;
+        switch (x){
+            case 1:
+                System.out.println("case 1");
+                break;
+            case 2:
+                System.out.println ("case 2");
+            case 3:
+                System.out.println("case 3");
+            default:
+                System.out.println("no match");
+
+        }
+        String name ="aman";
+
+        switch (name){
+            case "aman":
+                System.out.println("case 1");
+                break;
+            case "ajay":
+                System.out.println("case 2");
+            case "virat":
+                System.out.println("case 3");
+        }
+
     }
 }
---
+++ sw.java
@@ -0,0 +1,18 @@
+public class sw {
+    static void main(String[] args) {
+        String a = "anbmdfasdfasdfafsdfasdfsdfsdfsdf";
+       switch (a){
+           case "anbm" :
+               System.out.println("case 1");
+               break;
+           case "amit":
+               System.out.println("case 2");
+               break;
+           case "aman" :
+               System.out.println("case 3");
+           default:
+               System.out.println("not match any case ");
+
+       }
+    }
+}
